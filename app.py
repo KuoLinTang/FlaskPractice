c220dae9dcdb36516e26fed01862846ce6023fd0
KuoLinTang <linkuotang1997@gmail.com>
2023-06-18 23:18:34 +0100

    testing passing parameters using URL string and arguments

--- app.py
+++ app.py
@@ -14,5 +14,18 @@ def home():
     return render_template('homepage.html')
 
 
+# testing passing parameters using URL string
+@app.route('/home/test/<username>')
+def print_username(username):
+    return username
+
+
+# testing passing parameters using arguments
+@app.route('/home/test/')
+def request_username():
+    username = request.args.get("username")
+    return username
+
+
 if __name__ == "__main__":
     app.run(debug=True)
