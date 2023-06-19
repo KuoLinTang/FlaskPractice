7665825c9ad0349b1b4d08651901eb5e02defd68
KuoLinTang <linkuotang1997@gmail.com>
2023-06-19 18:36:19 +0100

    practicing how to pass arguments into a HTML file

--- app.py
+++ app.py
@@ -15,17 +15,25 @@ def home():
 
 
 # testing passing parameters using URL string
-@app.route('/home/test/<username>/')
+@app.route('/home/get_request_params/<username>/')
 def print_username(username):
     return f'USERNAME: {username}'
 
 
 # testing passing parameters using arguments
-@app.route('/home/test/')
+@app.route('/home/get_request_params/')
 def request_username():
     username = request.args.get("username")
     password = request.args.get("password")
     return f'USERNAME: {username} AND PASSWORD: {password}'
+
+
+# practicing passing parameters to a template
+@app.route('/home/pass_args_to_html/')
+def testing_html_parameter():
+    name = request.args.get('name')
+    username = request.args.get("username")
+    return render_template('page_with_parameter.html', name=name, username=username)
 
 
 if __name__ == "__main__":
